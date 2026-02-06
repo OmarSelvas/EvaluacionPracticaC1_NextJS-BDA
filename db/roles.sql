400c115222ae438aa4f7cfa079bc6c60cdb29131
0marS3lvas <omarselvas153@gmail.com>
2026-02-06 02:26:55 -0600

    avance

--- db/roles.sql
+++ db/roles.sql
@@ -1,29 +1,20 @@
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'app') THEN
-        CREATE ROLE app WITH LOGIN PASSWORD 'app_password_2024';
-    END IF;
-END
-$$;
-
-ALTER ROLE app WITH NOSUPERUSER NOCREATEDB NOCREATEROLE;
-REVOKE ALL ON SCHEMA public FROM app;
-REVOKE ALL ON ALL TABLES IN SCHEMA public FROM app;
-REVOKE ALL ON ALL SEQUENCES IN SCHEMA public FROM app;
-GRANT USAGE ON SCHEMA public TO app;
-GRANT CONNECT ON DATABASE postgres TO app;
-GRANT SELECT ON vw_course_performance TO app;
-GRANT SELECT ON vw_teacher_load TO app;
-GRANT SELECT ON vw_students_at_risk TO app;
-GRANT SELECT ON vw_attendance_by_group TO app;
-GRANT SELECT ON vw_rank_students TO app;
-
-ALTER DEFAULT PRIVILEGES IN SCHEMA public
-    GRANT SELECT ON TABLES TO app;
-SELECT 
-    table_schema,
-    table_name,
-    privilege_type
-FROM information_schema.table_privileges
-WHERE grantee = 'app'
-ORDER BY table_name;
+CREATE ROLE usuario_vistas WITH 
+    LOGIN 
+    PASSWORD 'Password123'
+    NOSUPERUSER
+    NOCREATEDB
+    NOCREATEROLE
+    NOINHERIT;
+GRANT CONNECT ON DATABASE db_awos TO view_user;
+GRANT USAGE ON SCHEMA public TO view_user;
+-- Permisos 
+GRANT SELECT ON 
+    vw_course_performance,
+    vw_teacher_load,
+    vw_students_at_risk,
+    vw_attendance_by_group,
+    vw_rank_students
+TO view_user;
+-- quitaar los permisos innecesarios
+REVOKE CREATE ON SCHEMA public FROM public;
+REVOKE ALL ON DATABASE db_awos FROM public;
